ef1ef5194e906ee2efa1bb47bcb0f9662dfcab8a
panditdhamdhere <dhamdherepandit@gmail.com>
2023-04-18 11:38:02 +0530

    footer updated

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
-import React from "react";
+// import React from "react";
 
-const Footer = () => {
-  return (
-    <div>
-      <p>2023 Pandit Dhamdhere</p>
-    </div>
-  );
-};
+// const Footer = () => {
+//   return (
+//     <div>
+//       <p>2023 Pandit Dhamdhere</p>
+//     </div>
+//   );
+// };
 
-export default Footer;
+// export default Footer;
